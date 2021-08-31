500d7789115295e63a368597f848fd9e60413c7d
IdanHerman12 <idanh156@gmail.com>
2021-08-31 19:31:32 +0300

    formula 1 winners counting the wins

--- code.js
+++ code.js
@@ -1,1 +1,32 @@
+const formula1Champions = [
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Alonso",
+    "Alonso",
+    "Räikkönen",
+    "Hamilton",
+    "Button",
+    "Vettel",
+    "Vettel",
+    "Vettel",
+    "Vettel",
+    "Hamilton",
+    "Hamilton",
+    "Rosberg",
+    "Hamilton",
+    "Hamilton",
+    "Hamilton",
+    "Hamilton"
+];
+let winners={}
 
+function countWin(driver) {
+   if(formula1Champions.hasOwnProperty(driver)){
+       winners[driver]=formula1Champions.forEach(driver);
+   }
+}
+
+console.log(formula1Champions.forEach(countWin));
